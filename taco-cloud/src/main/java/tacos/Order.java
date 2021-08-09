def73907f4b510b96e4e2830dc06e8d6a9653f38
Joode <Joode@jshin.innofactory.net>
2021-08-09 23:54:23 +0900

    스프링 공부

--- taco-cloud/src/main/java/tacos/Order.java
+++ taco-cloud/src/main/java/tacos/Order.java
@@ -1,12 +1,22 @@
 package tacos;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
 
 import lombok.Data;
 
 @Data
 public class Order {
+	
+	private Long id;
+	private Date createAt;
 
 	@NotBlank(message="Name is required")
 	private String deliveryName;
@@ -32,5 +42,11 @@ public class Order {
 	
 	@Digits(integer=3, fraction=0, message="Invalid CVV")
 	private String ccCVV;
+	
+	private List<Taco> tacos = new ArrayList<>();
+	
+	public void addDesign(Taco design) {
+		this.tacos.add(design);
+	}
 	
 }
